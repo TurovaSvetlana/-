a555e0e19b1b64266b1f86f822a5aa6e20f8857a
TurovaSvetlana <turov-74@mail.ru>
2022-01-26 18:52:17 +0700

    начало1

--- дз2/main.py
+++ дз2/main.py
@@ -23,6 +23,8 @@ for f in m:
         print(f)
 
 
+st='пир мор молоко хлеб снег компьютер'
+print(st.split())
 
 
 
@@ -31,18 +33,3 @@ for f in m:
 
 
 
-
-
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
