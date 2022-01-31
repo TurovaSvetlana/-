dd43df64377351eec1036c604c7fbe1beb3b78e2
TurovaSvetlana <turov-74@mail.ru>
2022-01-31 20:29:37 +0700

    add changes

---
+++ дз3/зад2.py
@@ -0,0 +1,9 @@
+
+def func(name, fameli, bunth, residente, emai, number):
+    return name + " " + fameli \
+           + " " + bunth + " " + residente \
+           + " " + emai + " " + number
+
+print(func(name='Светлана', fameli='Турова',
+    bunth='1975',  residente='Дзержинское',
+    emai='turov-74@mai.ru',  number='8902950481'))
